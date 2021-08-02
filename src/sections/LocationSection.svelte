703c25706754ae64de7de9c55eded1784efefa17
ssmg <ssmg.sg@gmail.com>
2021-08-02 13:53:31 -0600

    Create LocationSection

---
+++ src/sections/LocationSection.svelte
@@ -0,0 +1,29 @@
+<section>
+  <h3 id="ubicaciones">Nuestra ubicación</h3>
+  <p>
+    ¿No sabes dónde estamos ubicados? Te presentamos un mapa para que nos
+    encuentres.
+  </p>
+  <div>
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5460.960812976816!2d-90.49533729306428!3d14.571564864770414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a32d77f56b2f%3A0x629c97a36b19679d!2sAntojitos%20Del%20Parque!5e0!3m2!1sen!2sgt!4v1627923438805!5m2!1sen!2sgt"
+      width="100%"
+      height="450"
+      style="border:0;"
+      allowfullscreen=""
+      loading="lazy"
+      title="Ubicación en mapa de Antojitos del Parque"
+    />
+  </div>
+</section>
+
+<style>
+  div {
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 6px;
+    overflow: hidden;
+    box-shadow: var(--box-shadow);
+  }
+</style>
