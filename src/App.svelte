a7c7679aafc3782f71e6b423d58c3874732313f9
ssmg <ssmg.sg@gmail.com>
2021-08-02 10:33:15 -0600

    Create main section and add buttons styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,32 @@
-<div id="app" />
+<script>
+  import MainSection from './sections/MainSection.svelte'
+</script>
+
+<div id="app">
+  <div class="navbar"><nav>Hola</nav></div>
+  <main>
+    <MainSection />
+  </main>
+</div>
+
+<!-- SECTIONS
+  - Ubicación
+  - Horario
+  - Menu
+  - Redes sociales
+ -->
+<style>
+  #app {
+    font-family: 'Inter';
+    display: grid;
+    grid-template:
+      'navbar'
+      'main';
+  }
+
+  :global(.section-title) {
+    font-family: 'Yanone Kaffeesatz';
+    font-weight: 700;
+    font-size: 2.5rem;
+  }
+</style>
---
+++ src/sections/MainSection.svelte
@@ -0,0 +1,58 @@
+<div class="section">
+  <div class="container">
+    <div class="title">
+      <h1>Antojitos del Parque</h1>
+      <button class="button">Conócenos</button>
+    </div>
+    <div class="spacer" />
+    <img src="/images/logo.svg" alt="Antojitos del Parque" />
+  </div>
+</div>
+
+<style>
+  .section {
+    padding-top: 50px;
+    padding-bottom: 50px;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .spacer {
+    width: 50px;
+    height: 50px;
+  }
+
+  .title {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  @media screen and (min-width: 768px) {
+    .container {
+      flex-direction: row;
+    }
+
+    .title {
+      align-items: flex-start;
+    }
+  }
+
+  h1 {
+    font-family: 'Yanone Kaffeesatz';
+    font-weight: 700;
+    font-size: 5em;
+    text-align: center;
+    margin: 0;
+    margin-bottom: 35px;
+  }
+
+  img {
+    width: 15em;
+  }
+</style>
