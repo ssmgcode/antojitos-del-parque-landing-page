0ea0ce15bd4e990cb0456df6c9626f03b82e59c0
ssmg <ssmg.sg@gmail.com>
2021-08-02 10:48:06 -0600

    Create header bar

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,17 @@
+<nav>
+  <img src="/images/letters.svg" alt="Antojitos del Parque" />
+</nav>
+
+<style>
+  nav {
+    background: #000000;
+    padding: 10px 0;
+    display: flex;
+    justify-content: center;
+    grid-area: navbar;
+  }
+
+  img {
+    height: 50px;
+  }
+</style>
